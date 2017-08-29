3bfea882ca53a4417e2c0693bc66b4594cf22094
formation <icebrg07@live.fr>
2017-08-29 12:06:30 +0200

    dans add.html.twig => on a les filtres(pipe) dans les boucles et {{ yesterday }}// on a aussi les tag: if et for// recuperation tableau

--- src/AppBundle/Controller/calculatorController.php
+++ src/AppBundle/Controller/calculatorController.php
@@ -18,8 +18,15 @@ class calculatorController extends Controller
     public function addAction($n1, $n2)
 
     {
+        $clients=[
+            ["nom"=>"Pierre", "id"=>"3", "email"=>"pierro.p@gmail.com"],
+            ["nom"=>"mwana", "prenom"=>"mayi", "id"=>"1", "email"=>"mwana_mayi@gtk.net"]
+        ];
         $result=$n1 + $n2;
-        return $this->render(':calculator:add.html.twig', array("n1"=>$n1, "n2"=>$n2,"result"=>$result));
+        return $this->render(':calculator:add.html.twig', array("n1"=>$n1, "n2"=>$n2,"result"=>$result, 'cltr'=>$this,
+                                "yesterday"=> new \DateTime("today -1 day"), "list"=> [4,5,6,1,2,3,7,8,9],"clients"=>$clients
+
+        ));
     }
 
 }
